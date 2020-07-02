15e549060fde2a7a840355bef7c6e68c150bede5
Leonard Tan <tetratorus@gmail.com>
2020-07-02 15:56:30 +0800

    Add working bindings

--- types/httpHelpers.d.ts
+++ types/httpHelpers.d.ts
@@ -1,20 +1,19 @@
-export as namespace HttpHelpers
-declare function getAPIKey(): string;
-declare function setAPIKey(a: string)
-declare function clearAPIKey()
-declare function promiseTimeout<T>(ms:Number, promise: Promise<T>): Promise<T>
+export function getAPIKey(): string;
+export function setAPIKey(a: string)
+export function clearAPIKey()
+export function promiseTimeout<T>(ms:Number, promise: Promise<T>): Promise<T>
 interface CustomOptions {
     useAPIKey?: boolean;
     isUrlEncodedData?: boolean;
     timeout?: Number;
 }
 interface Data {}
-declare function get(url: RequestInfo, options?: RequestInit, customOptions?: CustomOptions): Promise<Response>;
-declare function post(url: RequestInfo, data?: Data, options?: RequestInit, customOptions?: CustomOptions): Promise<Response>;
-declare function patch(url: RequestInfo, data?: Data, options?: RequestInit, customOptions?: CustomOptions): Promise<Response>;
-declare function remove(url: RequestInfo, data?: Data, options?: RequestInit, customOptions?: CustomOptions): Promise<Response>;
+export function get(url: RequestInfo, options?: RequestInit, customOptions?: CustomOptions): Promise<Response>;
+export function post(url: RequestInfo, data?: Data, options?: RequestInit, customOptions?: CustomOptions): Promise<Response>;
+export function patch(url: RequestInfo, data?: Data, options?: RequestInit, customOptions?: CustomOptions): Promise<Response>;
+export function remove(url: RequestInfo, data?: Data, options?: RequestInit, customOptions?: CustomOptions): Promise<Response>;
 interface RPCParams {}
-declare function generateJsonRPCObject(method: string, params: RPCParams)
-declare function promiseRace(url: string, options?: RequestInit, timeout?: Number)
+export function generateJsonRPCObject(method: string, params: RPCParams)
+export function promiseRace(url: string, options?: RequestInit, timeout?: Number)
 
 export {}
